0b9465ec5e5b4df7d24fe99c65f27a45c90553a2
B-a-l-aj-i <balajivijay679@gmail.com>
2026-02-15 19:48:08 +0530

    Refactor project to use Vite instead of Create React App; removed public/index.html, src/App.js, and src/index.js files. Updated package.json and package-lock.json to include Vite and related dependencies.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Reactdom from "react-dom";
-import App from "./App";
-let root=Reactdom.createRoot(document.getElementById("root"));
-
-root.render(
-    <App/>
-)
